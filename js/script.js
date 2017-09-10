3afafe229bcc03d74953d14a1f51bba1de25f267
Van Hung <o0ohung4499o0o@gmail.com>
2017-09-10 14:58:43 +0700

    wikipedia viewer

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var apiURL = "https://en.wikipedia.org/w/api.php?callback=?";
+
+$(document).ready(() => {
+    $('#search-btn').click(search);
+
+    $('#search-text').keydown(event => {
+        
+        if (event.keyCode == 13) {
+            console.log("enter");
+            search();
+        }
+    });
+});
+
+var search = () => {
+    $("#result-div").empty();
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + $("#search-text").val() + "&prop=info&inprop=url&utf8=&format=json",
+        dataType: "jsonp",
+        success: data => {
+            console.log(data);
+            resArr = data.query.search;
+            resArr.forEach(element => {
+                $('#result-div').append(
+                    "<div id='result' class='results'><h5><a href='#' target= '_blank'>" + element["title"] + "</a></h5><p = class='extract'>" + element["snippet"] + "</p></div>"
+                );
+            })
+        }
+    });
+}
